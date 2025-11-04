6f63209614f4ec715383709fb6693bb26bd85618
pprusaczyk <pawlop1112@gmail.com>
2025-11-04 20:55:41 +0100

    Update transportujFlota.hpp

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -1,9 +1,29 @@
 #pragma once
-
+#include "Statki.hpp"
 #include "Stocznia.hpp"
+#include <iostream>
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    // Jeżeli towar == 0 nie tworzymy żadnego statku
+    if (towar == 0) {
+        return 0;
+    }
+
+    Stocznia stocznia{};
+    unsigned int przetransportowano = 0;
+    unsigned int licznik_zaglowcow = 0;
+
+    // Pętla kontynuuje, dopóki nie przetransportujemy co najmniej wymaganej ilości towaru.
+    while (przetransportowano < towar) {
+        Statek* nowy_statek = stocznia();
+        unsigned int transport_statku = nowy_statek->transportuj();
+        przetransportowano += transport_statku;
+        Zaglowiec* zaglowiec = dynamic_cast<Zaglowiec*>(nowy_statek);
+        if (zaglowiec != nullptr) {
+            licznik_zaglowcow++;
+        }
+        delete nowy_statek;
+    }
+    return licznik_zaglowcow;
 }
